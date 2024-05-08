0332612c46719b31a0afaf8178d566b58418fc10
Ali Abdollahian Noghabi <a.noghabi2002@gmail.com>
2024-05-08 19:46:59 +0330

    read input from file
    read inputs from input.txt file

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 #[derive(Clone, Debug)]
 struct Task {
     pid: u32,  // Process ID
@@ -10,6 +13,7 @@ struct Task {
 
 // FCFS Scheduler Function
 fn fcfs(mut tasks: Vec<Task>) -> Vec<Task> {
+    println!("FCFS (First Come First Serve) Output:");
     tasks.sort_by_key(|task| task.time_arrival);
     let mut current_time = 0;
     for task in tasks.iter_mut() {
@@ -37,22 +41,47 @@ fn calculate_metrics(tasks: &[Task]) -> (f64, f64) {
     (average_waiting_time, average_turnaround_time)
 }
 
-// Main entry point of the program
+// read task from input.txt file
+fn read_tasks_from_file(file_path: &str) -> io::Result<Vec<Task>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let tasks = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter_map(|line| {
+            let parts: Vec<u32> = line.split_whitespace()
+                                      .filter_map(|word| word.parse::<u32>().ok())
+                                      .collect();
+            if parts.len() == 3 {
+                Some(Task {
+                    pid: parts[0],
+                    time_arrival: parts[1],
+                    burst_time: parts[2],
+                    remaining_time: parts[2],
+                    start_time: None,
+                    finish_time: None,
+                })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(tasks)
+}
+
+//Main function
 fn main() {
-    let tasks = vec![
-        Task { pid: 1, time_arrival: 0, burst_time: 4, remaining_time: 4, start_time: None, finish_time: None },
-        Task { pid: 2, time_arrival: 1, burst_time: 5, remaining_time: 5, start_time: None, finish_time: None },
-        Task { pid: 3, time_arrival: 2, burst_time: 2, remaining_time: 2, start_time: None, finish_time: None },
-        Task { pid: 4, time_arrival: 3, burst_time: 1, remaining_time: 1, start_time: None, finish_time: None },
-        Task { pid: 5, time_arrival: 4, burst_time: 3, remaining_time: 3, start_time: None, finish_time: None },
-        Task { pid: 6, time_arrival: 5, burst_time: 4, remaining_time: 4, start_time: None, finish_time: None },
-    ];
-
-    // Testing FCFS scheduling
-    let completed_fcfs = fcfs(tasks.clone());
-    let (fcfs_avg_wait, fcfs_avg_turn) = calculate_metrics(&completed_fcfs);
-    println!("FCFS Average Waiting Time: {:.2}, Average Turnaround Time: {:.2}", fcfs_avg_wait, fcfs_avg_turn);
-
-    //TODO: SRTF scheduling 
-    //TODO: Round Robin scheduling
+    let tasks_file_path = "input.txt";
+    match read_tasks_from_file(tasks_file_path) {
+        Ok(tasks) => {
+            let completed_fcfs = fcfs(tasks.clone());
+            let (fcfs_avg_wait, fcfs_avg_turn) = calculate_metrics(&completed_fcfs);
+            println!("FCFS Average Waiting Time: {:.2}, Average Turnaround Time: {:.2}", fcfs_avg_wait, fcfs_avg_turn);
+        },
+        Err(e) => {
+            eprintln!("Failed to read tasks from {}: {}", tasks_file_path, e);
+        }
+    }
 }
